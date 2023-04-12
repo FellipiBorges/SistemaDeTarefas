7806e3b93d2019773ddf355fef6eb2520f2d883a
FellipiBorges <fellipiborgesmc@gmail.com>
2023-04-12 13:08:59 -0300

    Banco de dados

---
+++ SistemaDeTarefas/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaDeTarefas.Data;
+using SistemaDeTarefas.Models;
+using SistemaDeTarefas.Repositorios.Interfaces;
+
+namespace SistemaDeTarefas.Repositorios
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private readonly SistemaTarefasDBContex _dbContext;
+        public UsuarioRepositorio(SistemaTarefasDBContex sistemaTarefasDBContex)
+        {
+            _dbContext = sistemaTarefasDBContex;
+        }
+        public async Task<UsuarioModel> BuscarPorId(int id)
+        {
+            return await _dbContext.Usuarios.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<UsuarioModel>> BuscarTodosUsuarios()
+        {
+            return await _dbContext.Usuarios.ToListAsync(x => x.Id == id);
+        }
+
+        public async Task<UsuarioModel> Adicionar(UsuarioModel usuario)
+        {
+            _dbContext.Usuarios.Add(usuario);
+            _dbContext.SaveChanges();
+
+            return usuario;
+        }
+
+        public Task<bool> Apagar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
